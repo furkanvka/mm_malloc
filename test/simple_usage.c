20343927a622c1b74e611445e880953bbf9cc2e4
furkanvka <hamleniyap@gmail.com>
2025-08-26 20:33:41 +0300

    cmake_fix

--- test/simple_usage.c
+++ test/simple_usage.c
@@ -1,24 +1,25 @@
 #include "my_malloc.h"
 #include <stdio.h>
 
-
 int main() {
-
-
     int* list = (int*)mymalloc(sizeof(int) * 5);
-
-    for (int i = 0; i < 5; i++)
-    {
-        *(list+i) = i;
+    
+    if (list == NULL) {
+        printf("Bellek ayrilamadi!\n");
+        return 1;
     }
 
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d ",*(list+i));
+    for (int i = 0; i < 5; i++) {
+        *(list + i) = i;
     }
-
+    
+    printf("Bellekteki degerler:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", *(list + i)); 
+    }
     printf("\n");
 
-    
-    
+    myfree(list);
+
+    return 0;
 }
